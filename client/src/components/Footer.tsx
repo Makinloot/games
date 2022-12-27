e5ad272fd082bbb7dd7b396c96176e513b85aab5
Tornike <itorn9777@gmail.com>
2022-12-27 21:26:45 +0400

    created footer

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Footer = () => {
+
+  const handleSubmit = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+  }
+
+  return (
+    <div className="Footer">
+      <div className="container">
+        <div className="Footer-wrapper">
+          <div className="Footer-search flex-row">
+            <h2>search here</h2>
+            <form action="#" onSubmit={handleSubmit}>
+              <input type="text" placeholder="Enter game here" required />
+              <input type="submit" value="Enter" />
+            </form>
+          </div>
+          <div className="Footer-socials flex-row">
+            <nav className="Footer-menu">
+              <a className="footer-link" href="#">home</a>
+              <a className="footer-link" href="#">about us</a>
+              <a className="footer-link" href="#">contact</a>
+              <a className="footer-link" href="#">partners</a>
+            </nav>
+            <div className="socials flex-row">
+              <a href="https://github.com/Makinloot" title="github" target="_blank">
+                <i className="fa-brands fa-square-github"></i>
+              </a>
+              <a href="https://www.facebook.com/Meta1head/" title="github" target="_blank">
+                <i className="fa-brands fa-square-facebook"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
