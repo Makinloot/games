73d96b5396703b7a8a18b21d2caa31bb987edf38
Tornike <itorn9777@gmail.com>
2022-12-28 02:12:53 +0400

    changed header menu logic

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,27 +1,27 @@
-import React, { useState, useEffect, useRef, MouseEvent } from 'react';
-import logo from '../logo.png';
+import React, { useState, useEffect, useRef, MouseEvent } from "react";
+import logo from "../logo.png";
 
 const Header = () => {
-
   const [open, setOpen] = useState(false);
   const menuRef: any = useRef();
-  
+  const { width } = document.body.getBoundingClientRect();
+
   useEffect(() => {
-    const handleClass = (e: React.MouseEvent<Document, MouseEvent | TouchEvent>): void => {
-      if(!menuRef.current.contains(e.target)) {
+    const handleClass = (
+      e: React.MouseEvent<Document, MouseEvent | TouchEvent>
+    ): void => {
+      if (!menuRef.current.contains(e.target)) {
         setOpen(false);
       }
-    }
+    };
 
-    document.addEventListener('mousedown', handleClass);
+    document.addEventListener("mousedown", handleClass);
 
     return () => {
-      document.removeEventListener('mousedown', handleClass);
-    }
-  });
+      document.removeEventListener("mousedown", handleClass);
+    };
+  }, [width]);
 
-  const { width } = document.body.getBoundingClientRect();
-  
   return (
     <div className="Header">
       <div className="container">
@@ -29,36 +29,34 @@ const Header = () => {
           <a href="/" className="Header-logo">
             <img src={logo} alt="logo" />
           </a>
-          {width > 480 ?
-            <nav className="Header-menu">
-              <a href="/">Home</a>
-              <a href="#">Browse</a>
-              <a href="#">Contact</a>
-            </nav>
-            :
-            <div className='Header-burger'>
-              <div 
-                className={open ? 'burger-menu flex-col active' : 'burger-menu flex-col'}
-                onClick={() => {setOpen(!open)}}
-              >
-                <div></div>
-                <div></div>
-                <div></div>
-              </div>
-              <div 
-                className="burger-menu-content"
-                ref={menuRef}
-              >
-                <a href="/">home</a>
-                <a href="#">browse</a>
-                <a href="#">contact</a>
-              </div>
+          <nav className="Header-menu">
+            <a href="/">Home</a>
+            <a href="#">Browse</a>
+            <a href="#">Contact</a>
+          </nav>
+          <div className="Header-burger">
+            <div
+              className={
+                open ? "burger-menu flex-col active" : "burger-menu flex-col"
+              }
+              onClick={() => {
+                setOpen(!open);
+              }}
+            >
+              <div></div>
+              <div></div>
+              <div></div>
             </div>
-          }
+            <div className="burger-menu-content" ref={menuRef}>
+              <a href="/">home</a>
+              <a href="#">browse</a>
+              <a href="#">contact</a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
